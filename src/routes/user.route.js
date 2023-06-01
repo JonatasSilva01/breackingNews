aa5fef4e9c79183fae43a35b490d90665e5003c2
JonatasSilva01 <jhownsantossilva@gmail.com>
2023-05-31 23:19:42 -0300

    enviando os requisitos de ler todos os usuarios e ler somente por ID

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,8 @@
 const route = require("express").Router();
 const userController = require('../controller/user.controller.js');
+
 route.post("/", userController.create);
+route.get("/", userController.findAll);
+route.get("/:id", userController.findById);
 
 module.exports = route;
