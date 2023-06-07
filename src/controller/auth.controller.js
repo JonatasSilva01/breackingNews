775afa1f9ee88274edfcb7b9413f6f1b156d4c83
JonatasSilva01 <jhownsantossilva@gmail.com>
2023-06-07 16:38:00 -0300

    send web token generate

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,25 +1,23 @@
 import bcrypt from "bcrypt";
-import { loginService } from "../services/auth.service.js";
+import { generateTokenService, loginService } from "../services/auth.service.js";
 
 const login = async (req, res) => {
   // tudo o que for pegar vai vir do body do corpo da req(request)
   const { email, password } = req.body;
   try {
-    // bucando o 'user' para poder comparar as senhas que somente o bcrypt sabe.
     const user = await loginService(email);
 
-    // se não existir retornar um status 404
-    if(!user) res.status(404).send({ message: "user or password not found!" });
+    if (!user) res.status(404).send({ message: "user or password not found!" });
 
-    // aqui posso usar o compareSync ou compare(porem tenho que utilizar o await)
     const isPasswordIsValid = bcrypt.compareSync(password, user.password);
 
-    // se a senha for digitada incorretamente retorna um bad request 400
     if (!isPasswordIsValid)
       res.status(404).send({ message: "user or password not found!" });
 
-    // ocorreu tudo bem!
-    res.send("login ok");
+    //gera o token e passa o token para o usuario q vence a cada 24 horas 
+    const token = generateTokenService(user.id);
+
+    res.send({ token });
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,12 @@
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
-const loginService = (email) => User.findOne({email: email}).select("+password");
+const loginService = (email) =>
+  User.findOne({ email: email }).select("+password");
+// faça uma pesquisa sobre MD5 para criar um texto para gerar uma chave unica para o genarate token
+// 86400 segundos é equivalente a 24horas
+const generateTokenService = (id) =>
+  jwt.sign({ id: id }, process.env.SECRET_JWT, { expiresIn: 86400 });
 
-export {loginService};
+export { loginService, generateTokenService };
