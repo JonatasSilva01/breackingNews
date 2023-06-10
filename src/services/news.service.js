e66aca27e958d5083ff4b692f271b57bc6bdae43
JonatasSilva01 <jhownsantossilva@gmail.com>
2023-06-10 17:37:55 -0300

    up

--- src/services/news.service.js
+++ src/services/news.service.js
@@ -3,4 +3,6 @@ import News from "../models/News.js";
 export const createService = (body) => News.create(body);
 export const findAllService = (offset, limit) =>
   News.find().sort({ _id: -1 }).skip(offset).limit(limit).populate("user");
+// populate = relashion ship
 export const contNews = () => News.countDocuments();
+// cont Document ele conta quantos documentos tem no banco de dados;
